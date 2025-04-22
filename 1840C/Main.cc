aff29a221d3e31c0598c1c17adba8c1a2a0b823e
aidan-palmer <aidan.palmer@colostate.edu>
2025-04-21 18:10:45 -0600

    update

--- 1840C/Main.cc
+++ 1840C/Main.cc
@@ -1,11 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+uint64_t fact(int x) {
+    uint64_t result = 1;
+    for (int i = 2; i <= x; i++) {
+        result *= i;
+    }
+    return result;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t, n, i, j, k, q, l;
     cin >> t;
+    int results[t];
     for (i = 0; i < t; i++) {
         cin >> n >> k >> q;
         int a[n];
@@ -21,8 +31,20 @@ int main() {
             }
             j++;
         }
-
+        uint64_t total = 0;
+        for (int s : dates) {
+            if (s >= k) {
+                total += fact(s);
+            }
+        }
+        results[i] = total;
     }
-
+    for (i = 0; i < t; i++) {
+        cout << results[i] << '\n';
+    }
     return 0;
 }
+
+/**
+ * getting some wrong answers
+ */
