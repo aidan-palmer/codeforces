c2ffb3b7664e1da261245358b09324229500d2f3
Aidan Palmer <aidanhpalmer@gmail.com>
2025-09-05 11:07:42 -0600

    solved

--- 2132A/Main.cc
+++ 2132A/Main.cc
@@ -6,11 +6,27 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t, n, i, j;
+    int t, n, m, i, j;
     cin >> t;
+    string ans[t];
     for (i = 0; i < t; i++) {
         cin >> n;
+        string a, b, c;
+        cin >> a;
+        cin >> m;
+        cin >> b;
+        cin >> c;
+        for (j = 0; j < m; j++) {
+            if (c[j] == 'V') {
+                a = b[j] + a;
+            } else {
+                a += b[j];
+            }
+        }
+        ans[i] = a;
     }
-
+    for (i = 0; i < t; i++) {
+        cout << ans[i] << endn;
+    }
     return 0;
 }
